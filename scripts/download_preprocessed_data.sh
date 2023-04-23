f2b98c322e4d7fb1ca1c1d1266fc0a06b2a064e5
moritz-steiner <128054784+moritz-steiner@users.noreply.github.com>
2023-04-23 16:17:14 +0000

    Add files via upload

---
+++ scripts/download_preprocessed_data.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+data=$base/data
+shared_models=$base/shared_models
+
+# download preprocessed data
+
+wget https://files.ifi.uzh.ch/cl/archiv/2021/mt21/data.tar.gz -P $base
+tar -xzvf $base/data.tar.gz
+
+rm $base/data.tar.gz
+
+# download shared models (which, in this case, is only the vocabulary)
+
+wget https://files.ifi.uzh.ch/cl/archiv/2021/mt21/shared_models.tar.gz -P $base
+tar -xzvf $base/shared_models.tar.gz
+
+rm $base/shared_models.tar.gz
+
+# sizes
+echo "Sizes of data files:"
+wc -l $data/*
+
+echo "Sizes of shared_model files:"
+wc -l $shared_models/*
+
+# sanity checks
+echo "At this point, please make sure that 1) number of lines are as expected, 2) language suffixes are correct and 3) files are parallel"
---
+++ scripts/train.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+models=$base/models
+configs=$base/configs
+
+mkdir -p $models
+
+num_threads=10
+#device=0
+
+# measure time
+
+SECONDS=0
+
+logs=$base/logs
+
+model_name=deen_transformer_pre
+
+mkdir -p $logs
+
+mkdir -p $logs/$model_name
+
+OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+
+echo "time taken:"
+echo "$SECONDS seconds"
