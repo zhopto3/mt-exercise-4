abe8eac4d8daf1a1352caea010581eacae0661d8
Zachary William Hopton <zacharywilliam.hopton@uzh.ch>
2023-05-11 09:48:26 +0200

    Training script updated to do pre and post norm

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
+#Zachary William Hopton, 22-737-027
+#Allison Ponce de Leon Diaz 22-740-633
 scripts=$(dirname "$0")
 base=$scripts/..
 
@@ -8,16 +9,51 @@ configs=$base/configs
 
 mkdir -p $models
 
-num_threads=10
+#changed for the number of CPU cores we have.
+num_threads=2
 #device=0
 
 # measure time
+#The below is for to train the default model provided
+#SECONDS=0
+#
+#logs=$base/logs
+#
+#model_name=deen_transformer_regular
+#
+#mkdir -p $logs
+#
+#mkdir -p $logs/$model_name
+#
+#OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+#
+#echo "time taken:"
+#echo "$SECONDS seconds"
+
+echo "Now starting the pre-layer normalization" model training
 
 SECONDS=0
 
 logs=$base/logs
 
-model_name=deen_transformer_regular
+model_name=deen_transformer_pre
+
+mkdir -p $logs
+
+mkdir -p $logs/$model_name
+
+OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+
+echo "time taken:"
+echo "$SECONDS seconds"
+
+echo "Now starting the post-layer normalization" model training
+
+SECONDS=0
+
+logs=$base/logs
+
+model_name=deen_transformer_post
 
 mkdir -p $logs
 
